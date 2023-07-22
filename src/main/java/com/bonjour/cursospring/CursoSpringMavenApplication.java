3a4af0b653020f888d0aea6d78b2e8332d419dff
Rodrigo Bonjour <rodrigo@bonjourestamparia.com>
2023-07-22 18:00:27 -0300

    Associação muitos para muitos

--- src/main/java/com/bonjour/cursospring/CursoSpringMavenApplication.java
+++ src/main/java/com/bonjour/cursospring/CursoSpringMavenApplication.java
@@ -1,8 +1,6 @@
 package com.bonjour.cursospring;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.bonjour.cursospring.domain.Categoria;
+import com.bonjour.cursospring.domain.Produto;
 import com.bonjour.cursospring.repositories.CategoriaRepository;
+import com.bonjour.cursospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoSpringMavenApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository repo;
+
+	@Autowired
+	private ProdutoRepository produtoRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringMavenApplication.class, args);
@@ -27,8 +30,19 @@ public class CursoSpringMavenApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		repo.saveAll(Arrays.asList(cat1, cat2));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		repo.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
