06c01ef368341ca09f0f20ad430d50dbb15f4c05
Rodrigo Bonjour <rodrigo@bonjourestamparia.com>
2023-07-27 21:21:14 -0300

    Validação de email não repetido

--- src/main/java/com/bonjour/cursospring/repositories/ClienteRepository.java
+++ src/main/java/com/bonjour/cursospring/repositories/ClienteRepository.java
@@ -2,10 +2,19 @@ package com.bonjour.cursospring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bonjour.cursospring.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	
+	
+	/**
+	 * Ao criar um método findBy<Nome do atributo>, o Spring implementa automaticamente o método
+	 * @param email
+	 * @return
+	 */
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/bonjour/cursospring/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/bonjour/cursospring/services/validation/ClienteInsertValidator.java
@@ -3,14 +3,22 @@ package com.bonjour.cursospring.services.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bonjour.cursospring.domain.Cliente;
 import com.bonjour.cursospring.domain.enums.TipoCliente;
 import com.bonjour.cursospring.dto.ClienteNewDTO;
+import com.bonjour.cursospring.repositories.ClienteRepository;
 import com.bonjour.cursospring.resources.exception.FieldMessage;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>{
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 		
@@ -21,12 +29,16 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		List<FieldMessage> list = new ArrayList<FieldMessage>();
 		
 		if(TipoCliente.toEnum(objDTO.getTipo()).equals(TipoCliente.PESSOA_FISICA)) {
-			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+			//list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
 		}
 		
 		if(TipoCliente.toEnum(objDTO.getTipo()).equals(TipoCliente.PESSOA_JURIDICA)) {
-			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+			//list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDTO.getEmail());
+		if(aux != null)
+			list.add(new FieldMessage("email", "Email já cadastrado"));
 		
 		for(FieldMessage field : list) {
 			context.disableDefaultConstraintViolation();
